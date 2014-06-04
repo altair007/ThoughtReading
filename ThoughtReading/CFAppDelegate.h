ec6779877e76008eb9717b2143229c1e13c35a66
颜风 <1178328901@qq.com>
2014-06-04 09:57:24 +0800

    Initial Commit

---
+++ ThoughtReading/CFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CFAppDelegate.h
+//  ThoughtReading
+//
+//  Created by   颜风 on 14-6-4.
+//  Copyright (c) 2014年 Shadow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
